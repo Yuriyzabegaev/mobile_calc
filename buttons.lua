28cc653f689c40395418c2dadaf539f86d690493
Yuriy Zabegaev <zabegaev99@gmail.com>
2018-03-28 18:15:19 +0300

    minor

--- buttons.lua
+++ buttons.lua
@@ -1,21 +1,23 @@
 local Module = {}
 
 
-local function button(x, y)
-    local rect = display.newRect(x, y, 90, 90)
+local function button(text, x, y, sizeX, sizeY, color, colorTouch)
+    local rect = display.newRect(x, y, sizeX, sizeY)
     rect.fillColor = { 0, 0, 0, 1 }
+    rect.fillColor = color
+    rect.fillColorTouch = colorTouch
     rect:setFillColor(unpack((rect.fillColor)))
 
     local l = 0.8 * ((rect.contentWidth > rect.contentHeight) and rect.contentWidth or rect.contentHeight)
 
-    local text = display.newText("1", rect.x, rect.y, native.systemFont, l, "center")
+    local text = display.newText(text, rect.x, rect.y, native.systemFont, l, "center")
 
     local function changeButtonColor(event)
 
         print(event.phase)
         if event.phase == "began" then
             display.getCurrentStage():setFocus(event.target)
-            event.target:setFillColor(0, 0, 1, 1)
+            event.target:setFillColor(unpack(rect.fillColorTouch))
 
         elseif (event.phase == "ended" or event.phase == "cancelled") then
             display.getCurrentStage():setFocus(nil)
--- main.lua
+++ main.lua
@@ -10,6 +10,29 @@ background.y = display.contentCenterY
 
 local buttons = require("buttons")
 local make_button = buttons.button
+
+
+local function buttons ()
+    local buttonsList = {}
+    
+    for i = 1, nButtons do
+        local x = i*10 + 50
+        local y = x
+        table.insert(buttonsList, make_button(text, x, y, sizeX, sizeY, color, colorTouch))
+    end
+    return buttonsList
+end
+
+local arg_table = {1, 2, 3, 4}
+
+-- local sx = rect.contentWidth/text.contentWidth
+-- local sy = rect.contentHeight/text.contentHeight
+-- local scale = (sx < sy ) and sx or sy
+
+-- text:scale( scale, scale )
+
+-- native.getFontNames()
+
 -- local platform = display.newImageRect( "platform.png", 300, 50 )
 -- platform.x = display.contentCenterX
 -- platform.y = display.contentHeight - 25
@@ -34,24 +57,3 @@ local make_button = buttons.button
 -- local button1 = display.newRoundedRect()
 
 -- local rect_properties = {x=display.contentCenterX, y=display.contentCenterY, }
-
-local function buttons ()
-    local arg_table = {"1", "2", "3", "4", "5", "6", "7", "8", "9"}
-    local buttonsList = {}
-    for i = 1, nButtons do
-        local x = i*10 + 50
-        local y = x
-        table.insert(buttonsList, make_button(x, y))
-    end
-    return buttonsList
-end
-
-local arg_table = {1, 2, 3, 4}
-
--- local sx = rect.contentWidth/text.contentWidth
--- local sy = rect.contentHeight/text.contentHeight
--- local scale = (sx < sy ) and sx or sy
-
--- text:scale( scale, scale )
-
--- native.getFontNames()
